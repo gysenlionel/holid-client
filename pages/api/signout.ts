2e88af0046ed01f254d2f54b3c7c9e72c2dd3e4d
Gysen Lionel <gysenlionel@gmail.com>
2023-05-17 23:08:41 +0200

    fix signout vercel

--- pages/api/signout.ts
+++ pages/api/signout.ts
@@ -7,25 +7,16 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
-    // if (req.method === "POST"){
+    if (req.method === "POST"){
         try {
-            // const response = await axios.post(
-            //     `${environment.apiUrl}/api/auth/signout`,
-            //     {},
-            //     {
-            //       withCredentials: true,
-            //       headers: { cookie: req.headers.cookie },
-            //     }
-            //   );
-
-            const response = await axios.get(
-                `${environment.apiUrl}/api/auth/logout`,
+            const response = await axios.post(
+                `${environment.apiUrl}/api/auth/signout`,
+                {},
                 {
-                  headers: { cookie: req.headers.cookie },
                   withCredentials: true,
+                  headers: { cookie: req.headers.cookie },
                 }
               );
-              
               res.setHeader("Set-Cookie", response.headers['set-cookie'])
 
               res.status(200).json(response.data)
@@ -34,5 +25,5 @@ export default async function handler(
             res.status(500).json({statusCode:500, message:err?.response?.data})
         }
     }
-//   }
+  }
   
