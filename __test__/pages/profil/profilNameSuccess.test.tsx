4571e04afc5b3228823d716ad1541d3278f43a3a
Gysen Lionel <gysenlionel@gmail.com>
2023-10-02 22:51:54 +0200

    fix ts config

--- __test__/pages/profil/profilNameSuccess.test.tsx
+++ __test__/pages/profil/profilNameSuccess.test.tsx
@@ -47,36 +47,4 @@ describe("ReserveModal", () => {
       ).toBeInTheDocument();
     }, 500);
   });
-
-  // it("should fill the input address section and click on valid button", async () => {
-  //   mockRouter.push("/");
-  //   const { getByTestId, getAllByTestId, getByPlaceholderText, getByRole } =
-  //     render(
-  //       <UserProvider initialUser={user}>
-  //         <Profil user={user} />
-  //       </UserProvider>
-  //     );
-  //   const pencils = getAllByTestId("pencil");
-  //   await userEvent.click(pencils[1]);
-  //   const submits = getAllByTestId("submit");
-  //   delete user.firstname;
-  //   delete user.lastname;
-
-  //   // Name section
-  //   fireEvent.change(getByPlaceholderText(/Address/), {
-  //     target: { value: "Avenue du test 1" },
-  //   });
-  //   fireEvent.change(getByPlaceholderText(/City/), {
-  //     target: { value: "Brussels" },
-  //   });
-  //   const dropdownButton = screen.getByRole("button");
-  //   await userEvent.click(dropdownButton);
-  //   await userEvent.click(screen.getByText(/Belgium/));
-  //   await userEvent.click(submits[0]);
-  //   setTimeout(() => {
-  //     expect(
-  //       screen.getByText("Your profil has been updated")
-  //     ).toBeInTheDocument();
-  //   }, 1000);
-  // });
 });
